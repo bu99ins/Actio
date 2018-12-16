3a6e7f43c2eb146cf0b7f9b03ef453867a55528d
Yury Krasavin <buggins@live.com>
2018-12-17 00:45:09 +0300

    Subscribe Activities service to messages.

--- src/Actio.Common/RabbitMq/Extensions.cs
+++ src/Actio.Common/RabbitMq/Extensions.cs
@@ -2,7 +2,10 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Actio.Common.Commands;
 using Actio.Common.Events;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using RawRabbit;
+using RawRabbit.Instantiation;
 
 namespace Actio.Common.RabbitMq
 {
@@ -22,5 +25,19 @@ namespace Actio.Common.RabbitMq
 
         private static string GetQueName<T>()
             => $"{Assembly.GetEntryAssembly().GetName()}/{typeof(T).Name}";
+
+        public static void AddRabbitMq(this IServiceCollection services, IConfiguration configuration)
+        {
+            var options = new RabbitMqOptions();
+            var section = configuration.GetSection("rabbitmq");
+            section.Bind(options);
+
+            var client = RawRabbitFactory.CreateSingleton(new RawRabbitOptions
+            {
+                ClientConfiguration = options
+            });
+
+            services.AddSingleton<IBusClient>(_ => client);
+        }
     }
 }
