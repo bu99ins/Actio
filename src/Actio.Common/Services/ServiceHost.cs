9b46dd5912a1f5e326761117d85428cacd092eb8
Yury Krasavin <buggins@live.com>
2018-12-16 19:28:50 +0300

    Subscribe to RabbitMq commands and events.

--- src/Actio.Common/Services/ServiceHost.cs
+++ src/Actio.Common/Services/ServiceHost.cs
@@ -1,4 +1,7 @@
 using System;
+using Actio.Common.Commands;
+using Actio.Common.Events;
+using Actio.Common.RabbitMq;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -70,9 +73,29 @@ namespace Actio.Common.Services
                     _bus = bus;
                 }
 
+                public BusBuilder SubscribeToCommand<TCommand>() where TCommand : ICommand
+                {
+                    var handler = (ICommandHandler<TCommand>)_webHost.Services
+                        .GetService(typeof(ICommandHandler<TCommand>));
+
+                    _bus.WithCommandHandlerAsync(handler);
+
+                    return this;
+                }
+
+                public BusBuilder SubscribeToEvent<TEvent>() where TEvent : IEvent
+                {
+                    var handler = (IEventHandler<TEvent>)_webHost.Services
+                        .GetService(typeof(IEventHandler<TEvent>));
+
+                    _bus.WithEventHandlerAsync(handler);
+
+                    return this;
+                }
+
                 public override ServiceHost Build()
                 {
-                    throw new NotImplementedException();
+                    return new ServiceHost(_webHost);
                 }
             }
         }
